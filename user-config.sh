b7d78e56553f1530d4c0818fc20ca51d59e5baec
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-11-24 10:52:43 +0900

    feat(user-config): add alias for checking git status

--- user-config.sh
+++ user-config.sh
@@ -1,4 +1,5 @@
 alias gitlog='git log --all --decorate --oneline --graph'
+alias gita='git status'
 alias killsidebutton="xmodmap -e 'pointer = 1 2 3 4 5 6 7 0 0 0 0 0 0'"
 alias kucing="kitty --detach"
 alias koceng="xterm &"
